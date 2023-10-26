a33f428bda0b213b53222f252fd3d9c416b6d0c4
RikuGhi <rifkhairkhairur@gmail.com>
2023-10-26 10:18:19 +0700

    add: build action

--- src/main/data-source.ts
+++ src/main/data-source.ts
@@ -1,20 +1,21 @@
 import 'reflect-metadata'
 import { DataSource } from 'typeorm'
-// import { Photo } from '../entities/Photo'
-// import { PhotoMetadata } from '../entities/PhotoMetadata'
-// import { Author } from '../entities/Author'
-// import { Album } from '../entities/Album'
+import { Photo } from '../entities/Photo'
+import { PhotoMetadata } from '../entities/PhotoMetadata'
+import { Author } from '../entities/Author'
+import { Album } from '../entities/Album'
 import { Post } from '../entities/Post'
 // import { PostRefactoring1698153432139 } from '../migrations/1698153432139-PostRefactoring'
 import { CreatePosts1698213471347 } from '../migrations/1698213471347-CreatePosts'
 import { CreateComments1698216700137 } from '../migrations/1698216700137-CreateComments'
-import { app } from 'electron'
-import { join } from 'path'
+// import { app } from 'electron'
+// import { join } from 'path'
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
-  database: join(app.getPath('userData'), 'database/test_db.sqlite'),
-  entities: [Post],
+  database: 'database/test_db.sqlite',
+  // database: join(app.getPath('userData'), 'database/test_db.sqlite'),
+  entities: [Photo, PhotoMetadata, Author, Album, Post],
   // synchronize: true,
   logging: true,
   // migrations: [CreatePosts1698213471347]
